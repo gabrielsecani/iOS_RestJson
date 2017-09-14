c6651a803aae4af871747b11f4315f479b53fc7b
Gabriel Lucas <gabrielltr84@gmail.com>
2017-09-14 20:18:22 -0300

    branch inicial para exercicio de 20%

--- RestJson/ViewController.swift
+++ RestJson/ViewController.swift
@@ -18,63 +18,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var ender: UITextField!
     var session: URLSession?
     
-    @IBAction func exibir(_ sender: Any) {
+    
+    @IBAction func buscarCEP(_ sender: Any) {
+        
         let config = URLSessionConfiguration.default
         
         session = URLSession(configuration: config)
-        let url = URL(string: "https://parks-api.herokuapp.com/parks/57b702ae8560c511000ff348")
-        
-        let task = session?.dataTask(with: url!, completionHandler: { (data, response, error) in
-            // ações que serão efetuadas qunado
-            // a execução da task se completa
-            let texto = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
-             print(texto!)
-            if let nPQ = self.retornaNomePq(data: data!){
-                DispatchQueue.main.async {
-                    self.local.text = nPQ
-                }
-            }
-            if let ePQ = self.retornaEstadoParque(data: data!){
-                DispatchQueue.main.async {
-                    self.estado.text = ePQ
-                }
-            }
-        })
-        // a ln abaixo dispara a execução da task
-        task?.resume()
-    }
-    
-    func retornaNomePq(data: Data) -> String? {
-        var resposta:String?=nil
-        do{
-            let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String: AnyObject]
-            if let nomeParque = json["nome"] as? String{
-                resposta = nomeParque
-            }
-        }catch let error as NSError{
-            return "Falha as carregar: \(error.localizedDescription)"
-        }
-        return resposta
-    }
-    
-    func retornaEstadoParque(data: Data) -> String? {
-        var resposta:String?=nil
-        do{
-            let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String: AnyObject]
-            if let nomeEstado = json["estado"] as? String{
-                resposta = nomeEstado
-            }
-        }catch let error as NSError{
-            return "Falha as carregar: \(error.localizedDescription)"
-        }
-        return resposta
-    }
-    
-    @IBAction func buscaCEP(_ sender: Any) {
-        let config = URLSessionConfiguration.default
-        
-        session = URLSession(configuration: config)
-        let url = URL(string: "https://www.viacep.com.br/ws/09972020/json/")
+        let url = URL(string: "http://viacep.com.br/ws/01538000/json/")
         print(url!)
         let task = session?.dataTask(with: url!, completionHandler: { (data, response, error) in
             // ações que serão efetuadas qunado
